b00e6249307d93f89c1248f0f6eaef3229a7ee29
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-12-04 16:57:43 +0000

    Add session fetch tests

---
+++ bundler/tests/fixtures/beamline_sessions.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    `BLSession` (
+        `sessionId`,
+        `proposalId`,
+        `visit_number`
+    )
+VALUES (1002, 100, 1), (1003, 100, 2), (1004, 101, 1);
---
+++ bundler/tests/fixtures/session_membership.sql
@@ -0,0 +1,3 @@
+INSERT INTO
+    `Session_has_Person` (`sessionId`, `personId`)
+VALUES (1000, 10), (1001, 10), (1000, 11);
