a9140b7c5198268a44f9a3d59537a8ff955e76f8
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-12-07 15:52:55 +0000

    Crash on fetch errors

--- bundler/src/main.rs
+++ bundler/src/main.rs
@@ -75,41 +75,45 @@ async fn main() {
         .finish()
         .init();
 
-    let ispyb_pool = Arc::new(
-        MySqlPoolOptions::new()
-            .connect(args.database_url.as_str())
-            .await
-            .unwrap(),
-    );
+    let ispyb_pool = MySqlPoolOptions::new()
+        .connect(args.database_url.as_str())
+        .await
+        .unwrap();
     let current_bundle = Arc::new(RwLock::new(
         BundleFile::try_from(Bundle::fetch(NoMetadata, &ispyb_pool).await.unwrap()).unwrap(),
-    ));
-    tokio::task::spawn(update_bundle(
-        current_bundle.clone(),
-        ispyb_pool.clone(),
-        args.polling_interval.into(),
     ));
     let app = Router::new()
         .route("/bundle.tar.gz", get(bundle_endpoint))
         .layer(TraceLayer::new_for_http())
-        .with_state(current_bundle);
-    let socket_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, args.port));
+        .with_state(current_bundle.clone());
+
+    let mut tasks = tokio::task::JoinSet::new();
+    tasks.spawn(update_bundle(
+        current_bundle,
+        ispyb_pool,
+        args.polling_interval.into(),
+    ));
+    tasks.spawn(serve_app(args.port, app));
+    tasks.join_next().await.unwrap().unwrap()
+}
+
+async fn serve_app(port: u16, app: Router) {
+    let socket_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port));
     let listener = TcpListener::bind(socket_addr).await.unwrap();
-    serve(listener, app).await.unwrap();
+    serve(listener, app).await.unwrap()
 }
 
 async fn update_bundle(
     current_bundle: impl AsRef<RwLock<BundleFile<NoMetadata>>>,
-    ispyb_pool: impl AsRef<MySqlPool> + Clone,
+    ispyb_pool: MySqlPool,
     polling_interval: Duration,
 ) {
     let mut next_fetch = Instant::now().add(polling_interval);
+
     loop {
         sleep_until(next_fetch).await;
         next_fetch = next_fetch.add(polling_interval);
-        let bundle = Bundle::fetch(NoMetadata, ispyb_pool.clone().as_ref())
-            .await
-            .unwrap();
+        let bundle = Bundle::fetch(NoMetadata, &ispyb_pool).await.unwrap();
         let bundle_file = BundleFile::try_from(bundle).unwrap();
         *current_bundle.as_ref().write().await = bundle_file;
     }
