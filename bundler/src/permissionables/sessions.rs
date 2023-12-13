224d3018a99442ded2091299a12fed8a3bf328e8
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-12-13 10:38:19 +0000

    Index users by their fedID

--- bundler/src/permissionables/sessions.rs
+++ bundler/src/permissionables/sessions.rs
@@ -3,7 +3,7 @@ use sqlx::{query_as, MySqlPool};
 use std::collections::HashMap;
 
 #[derive(Debug, Default, PartialEq, Eq, Serialize)]
-pub struct Sessions(HashMap<u32, Vec<(u32, u32)>>);
+pub struct Sessions(HashMap<String, Vec<(u32, u32)>>);
 
 impl Sessions {
     pub async fn fetch(ispyb_pool: &MySqlPool) -> Result<Self, sqlx::Error> {
@@ -11,15 +11,15 @@ impl Sessions {
             SessionRow,
             "
             SELECT
-                personId as person_id,
+                login as fed_id,
                 proposalId as proposal_id,
                 visit_number
             FROM Session_has_Person
-            JOIN BLSession
-            WHERE Session_has_Person.sessionId = BLSession.sessionId
+                INNER JOIN BLSession USING (sessionId)
+                INNER JOIN Person USING (personId)
             UNION
             SELECT
-                personId as person_id,
+                login as fed_id,
                 BLSession.proposalId as proposal_id,
                 visit_number
             FROM (
@@ -29,9 +29,8 @@ impl Sessions {
                     FROM
                         ProposalHasPerson
                 ) AS UniqueProposalHasPerson
-                CROSS JOIN BLSession
-            WHERE
-                UniqueProposalHasPerson.proposalId = BLSession.proposalId
+                CROSS JOIN BLSession USING (proposalId)
+                INNER JOIN Person USING (personId)
             "
         )
         .fetch_all(ispyb_pool)
@@ -42,7 +41,7 @@ impl Sessions {
 }
 
 struct SessionRow {
-    person_id: u32,
+    fed_id: Option<String>,
     proposal_id: u32,
     visit_number: Option<u32>,
 }
@@ -51,10 +50,12 @@ impl FromIterator<SessionRow> for Sessions {
     fn from_iter<T: IntoIterator<Item = SessionRow>>(iter: T) -> Self {
         let mut sessions = Self::default();
         for session_row in iter {
-            sessions.0.entry(session_row.person_id).or_default().push((
-                session_row.proposal_id,
-                session_row.visit_number.unwrap_or_default(),
-            ));
+            if let Some(fed_id) = session_row.fed_id {
+                sessions.0.entry(fed_id).or_default().push((
+                    session_row.proposal_id,
+                    session_row.visit_number.unwrap_or_default(),
+                ));
+            }
         }
         sessions
     }
@@ -64,7 +65,7 @@ impl FromIterator<SessionRow> for Sessions {
 mod tests {
     use super::Sessions;
     use sqlx::MySqlPool;
-    use std::collections::HashMap;
+    use std::collections::{HashMap, HashSet};
 
     #[sqlx::test(migrations = "tests/migrations")]
     async fn fetch_empty(ispyb_pool: MySqlPool) {
@@ -77,50 +78,82 @@ mod tests {
         migrations = "tests/migrations",
         fixtures(
             path = "../../tests/fixtures",
-            scripts("session_membership", "beamline_sessions")
+            scripts("session_membership", "beamline_sessions", "persons")
         )
     )]
     async fn fetch_direct(ispyb_pool: MySqlPool) {
         let sessions = Sessions::fetch(&ispyb_pool).await.unwrap();
-        let mut expected = Sessions(HashMap::new());
-        expected.0.insert(10, vec![(100, 1), (100, 2)]);
-        expected.0.insert(11, vec![(101, 1)]);
-        assert_eq!(expected, sessions);
+        let mut expected = HashMap::new();
+        expected.insert("foo".to_string(), vec![(100, 1), (100, 2)]);
+        expected.insert("bar".to_string(), vec![(101, 1)]);
+        assert_eq!(
+            expected,
+            sessions
+                .0
+                .into_iter()
+                .map(|(k, v)| (k, v.into_iter().collect()))
+                .collect()
+        );
     }
 
     #[sqlx::test(
         migrations = "tests/migrations",
         fixtures(
             path = "../../tests/fixtures",
-            scripts("proposal_membership", "beamline_sessions")
+            scripts("proposal_membership", "beamline_sessions", "persons")
         )
     )]
     async fn fetch_indirect(ispyb_pool: MySqlPool) {
         let sessions = Sessions::fetch(&ispyb_pool).await.unwrap();
-        let mut expected = Sessions(HashMap::new());
-        expected
-            .0
-            .insert(10, vec![(100, 1), (100, 2), (100, 3), (101, 1), (101, 2)]);
-        expected.0.insert(11, vec![(100, 1), (100, 2), (100, 3)]);
-        assert_eq!(expected, sessions);
+        let mut expected = HashMap::new();
+        expected.insert(
+            "foo".to_string(),
+            HashSet::from([(100, 1), (100, 2), (100, 3), (101, 1), (101, 2)]),
+        );
+        expected.insert(
+            "bar".to_string(),
+            HashSet::from([(100, 1), (100, 2), (100, 3)]),
+        );
+        assert_eq!(
+            expected,
+            sessions
+                .0
+                .into_iter()
+                .map(|(k, v)| (k, v.into_iter().collect()))
+                .collect()
+        );
     }
 
     #[sqlx::test(
         migrations = "tests/migrations",
         fixtures(
             path = "../../tests/fixtures",
-            scripts("session_membership", "proposal_membership", "beamline_sessions")
+            scripts(
+                "session_membership",
+                "proposal_membership",
+                "beamline_sessions",
+                "persons"
+            )
         )
     )]
     async fn fetch_both(ispyb_pool: MySqlPool) {
         let sessions = Sessions::fetch(&ispyb_pool).await.unwrap();
-        let mut expected = Sessions(HashMap::new());
-        expected
-            .0
-            .insert(10, vec![(100, 1), (100, 2), (100, 3), (101, 1), (101, 2)]);
-        expected
-            .0
-            .insert(11, vec![(101, 1), (100, 1), (100, 2), (100, 3)]);
-        assert_eq!(expected, sessions);
+        let mut expected = HashMap::new();
+        expected.insert(
+            "foo".to_string(),
+            HashSet::from([(100, 1), (100, 2), (100, 3), (101, 1), (101, 2)]),
+        );
+        expected.insert(
+            "bar".to_string(),
+            HashSet::from([(100, 1), (100, 2), (100, 3), (101, 1)]),
+        );
+        assert_eq!(
+            expected,
+            sessions
+                .0
+                .into_iter()
+                .map(|(k, v)| (k, v.into_iter().collect()))
+                .collect()
+        );
     }
 }
